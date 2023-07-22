ed7b1fda55c425c97b4582c644a77732beb43522
Savallera <phenixvn1985@gmail.com>
2023-07-22 14:22:50 +0300

    main page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+const mobileMenuBtn = document.querySelector(".content-header__btn");
 const mobileMenu = document.querySelector(".header");
 const mobileMenuOpen = () => {
     mobileMenu.classList.add("header_open");
-    mobileMenuBtn.classList.add("mobile-menu-btn_active");
+    mobileMenuBtn.classList.add("content-header__btn_active");
 };
 const mobileMenuClose = () => {
     mobileMenu.classList.remove("header_open");
-    mobileMenuBtn.classList.remove("mobile-menu-btn_active");
+    mobileMenuBtn.classList.remove("content-header__btn_active");
 };
 mobileMenuBtn.addEventListener("click", () => {
     if (mobileMenu.classList.contains("header_open")) {
